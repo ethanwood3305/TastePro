36406dea9966fb5ddc23fad47a944a57aa1d521b
Ethan Wood <50244760+ethanwood3305@users.noreply.github.com>
2024-11-15 18:37:32 +0000

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppRoutes from "./routes";
-import "./styles/global.css";
-
-ReactDOM.render(<AppRoutes />, document.getElementById("root"));
